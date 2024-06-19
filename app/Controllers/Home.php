c24ee1c0ce853236aa3d39dff3d44e29bb2f9910
Florin Pinta <xttrust@live.com>
2024-06-19 23:35:42 +0100

    Setup the codeigniter plus added front end html theme files

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,9 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
-        return view('welcome_message');
+class Home extends BaseController {
+
+    public function index() {
+        echo "Plain index function.";
     }
 }
