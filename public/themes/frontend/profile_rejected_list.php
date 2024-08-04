2ced250e6c7810ce143fa2e65da35eef7e007128
Florin Pinta <xttrust@live.com>
2024-08-04 12:58:54 +0100

    Added to profile rejected HTML

--- public/themes/frontend/profile_rejected_list.php
+++ public/themes/frontend/profile_rejected_list.php
@@ -1,1 +1,40 @@
-<p>rejected list goes here</p>
+<!-- Requests list -->
+<div class="row">
+    <div class="container my-4">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h2 class="text-danger">Rejected Requests List</h2>
+            <button class="btn btn-outline-danger">
+                <span class="icon-refresh"></span> Refresh Requests
+            </button>
+        </div>
+
+        <div class="list-group">
+            <!-- Accepted Item 1 -->
+            <div class="list-group-item d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 class="mb-1 text-dark">Call on me - Eric Prydz</h5>
+                    <p class="mb-1 text-secondary">Requested by: xttrust</p>
+                </div>
+                <div class="btn-group" role="group">
+                    <!-- Vote button -->
+                    <button type="button" class="btn btn-danger btn-small" title="Love this song">
+                        <span class="icon-heart"></span>
+                    </button>
+                    <!-- Play button -->
+                    <button type="button" class="btn btn-success btn-small" title="Play song">
+                        <span class="icon-play"></span>
+                    </button>
+                    <!-- Options button -->
+                    <button type="button" class="btn btn-dark btn-small" title="Options">
+                        <span class="icon-wrench"></span>
+                    </button>
+                </div>
+            </div>
+            <!-- End Accepted Item 1 -->
+
+        </div>
+    </div>
+
+
+</div>
+<!-- end Requests list -->
